4173d38582955dad764c499dea07e02ed0d05ccb
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-03-27 12:13:36 +0530

    Update SmartCampusAppliation.java

--- src/main/java/uk/ac/westminster/smartcampus/SmartCampusApplication.java
+++ src/main/java/uk/ac/westminster/smartcampus/SmartCampusApplication.java
@@ -1,7 +1,7 @@
 package uk.ac.westminster.smartcampus;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api/v1")
 public class SmartCampusApplication extends Application {
