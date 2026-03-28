59c40ca067f0a11ef48c7b272b911fa04401cdb8
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-03-28 15:16:34 +0530

    Update DiscoveryResource.java

--- src/main/java/uk/ac/westminster/smartcampus/DiscoveryResource.java
+++ src/main/java/uk/ac/westminster/smartcampus/DiscoveryResource.java
@@ -1,4 +1,12 @@
 package uk.ac.westminster.smartcampus;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.KinkedHashMp;
+import java.util.Map;
+
 public class DiscoveryResource {
 }
