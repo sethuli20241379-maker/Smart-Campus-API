1b120a14bb1c5f31cd621f0f8c60b7cd292e2585
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-03-28 15:17:18 +0530

    bugfix/fixed-spelling-errors-in-DiscoveryResource.java

--- src/main/java/uk/ac/westminster/smartcampus/DiscoveryResource.java
+++ src/main/java/uk/ac/westminster/smartcampus/DiscoveryResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.KinkedHashMp;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class DiscoveryResource {
