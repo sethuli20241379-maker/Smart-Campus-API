25bd818725fa28a6fd5b62a8ad3f96a2ddb0e597
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-09 13:56:49 +0530

    bugfix/spelling-error

--- src/main/java/uk/ac/westminster/smartcampus/Room.java
+++ src/main/java/uk/ac/westminster/smartcampus/Room.java
@@ -18,7 +18,7 @@ public class Room {
     }
 
     public String getId() { return id; }
-    public void setID(String id) { this.id = id;}
+    public void setId(String id) { this.id = id;}
 
     public String getName() { return name;}
     public void setName(String name) { this.name = name;}
