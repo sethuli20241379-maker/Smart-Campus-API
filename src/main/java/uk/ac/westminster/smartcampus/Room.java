a974b9a3c9962749e9ac4d14177b93c563986aba
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-09 13:51:58 +0530

    Update Room.java - Getters and Setters

--- src/main/java/uk/ac/westminster/smartcampus/Room.java
+++ src/main/java/uk/ac/westminster/smartcampus/Room.java
@@ -20,5 +20,6 @@ public class Room {
     public String getId() { return id; }
     public void setID(String id) { this.id = id;}
 
-    
+    public String getName() { return name;}
+    public void setName(String name) { this.name = name;}
 }
