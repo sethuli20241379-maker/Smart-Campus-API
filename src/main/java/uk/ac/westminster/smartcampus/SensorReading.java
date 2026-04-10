51da89d13bb6c6d385a2728601cf610915f94f30
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-10 20:31:20 +0530

    Update SensorReading.java

--- src/main/java/uk/ac/westminster/smartcampus/SensorReading.java
+++ src/main/java/uk/ac/westminster/smartcampus/SensorReading.java
@@ -3,4 +3,17 @@ package uk.ac.westminster.smartcampus;
 import java.time.LocalDateTime;
 
 public class SensorReading {
+    private double value;
+    private String timestamp;
+
+    public SensorReading() {
+        this.timestamp = LocalDateTime.now().toString();
+    }
+
+    public SensorReading(double value) {
+        this();
+        this.value = value;
+    }
+
+    
 }
