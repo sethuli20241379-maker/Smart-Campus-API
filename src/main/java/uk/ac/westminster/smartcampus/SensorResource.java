f4a23fa7d8e7ab1a1613e7df54adf13589e7beeb
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-10 16:19:09 +0530

    Create SensorResource.java

---
+++ src/main/java/uk/ac/westminster/smartcampus/SensorResource.java
@@ -0,0 +1,30 @@
+package uk.ac.westminster.smartcampus;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Path("/sensors")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorResource {
+
+    private static final Map<String, Sensor> sensors = new ConcurrentHashMap<>();
+
+    @POST
+    public Response registerSensor(Sensor sensor) {
+
+        if (sensor.getId() == null || sensors.containsKey(sensor.getId())) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Sensor ID is missing or already exists").build();
+        }
+
+        if (!RoomResource.getRoomsMap().containsKey(sensor.getRoomId())) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Error: The specified Room ID(" + sensor.getRoomId() + ") does not exist.")
+                    .build();
+        }
+    }
+}
