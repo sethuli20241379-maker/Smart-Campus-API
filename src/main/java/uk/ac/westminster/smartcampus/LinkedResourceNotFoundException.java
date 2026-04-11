2506d8e0d91b1770cc10f25586a9de2d7b01f0fc
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-11 18:29:21 +0530

    Create LinkedResourceNotFoundException.java

---
+++ src/main/java/uk/ac/westminster/smartcampus/LinkedResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package uk.ac.westminster.smartcampus;
+
+public class LinkedResourceNotFoundException  extends RuntimeException {
+    public LinkedResourceNotFoundException(String message) {
+        super(message);
+    }
+}
