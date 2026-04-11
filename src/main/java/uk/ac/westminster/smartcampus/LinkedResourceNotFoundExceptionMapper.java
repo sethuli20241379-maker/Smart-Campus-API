dcaebbe7cdb2d394d21f633d8f73ecde590bab85
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-11 18:35:01 +0530

    Update LinkedResourceNotFoundExceptionMapper.java

--- src/main/java/uk/ac/westminster/smartcampus/LinkedResourceNotFoundExceptionMapper.java
+++ src/main/java/uk/ac/westminster/smartcampus/LinkedResourceNotFoundExceptionMapper.java
@@ -1,4 +1,23 @@
 package uk.ac.westminster.smartcampus;
 
-public class LinkedResourceNotFoundExceptionMapper {
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException exception) {
+        ErrorMessage error = new ErrorMessage(
+                exception.getMessage(),
+                422
+        );
+
+        return Response.status(422)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
 }
