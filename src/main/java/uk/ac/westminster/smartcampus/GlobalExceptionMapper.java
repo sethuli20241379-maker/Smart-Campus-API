a633ab2222e61a7d6823d13cb6a2912d75d8469c
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-15 20:21:18 +0530

    Created GlobalExceptionMapper.java

--- src/main/java/uk/ac/westminster/smartcampus/GlobalExceptionMapper.java
+++ src/main/java/uk/ac/westminster/smartcampus/GlobalExceptionMapper.java
@@ -1,4 +1,30 @@
 package uk.ac.westminster.smartcampus;
 
-public class GlobalExceptionMapper {
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    private static final Logger LOGGER = Logger.getLogger(GlobalExceptionMapper.class.getName());
+
+    @Override
+    public Response toResponse(Throwable exception) {
+
+        LOGGER.log(Level.SEVERE, "Unexpected error occurred", exception);
+
+        ErrorMessage error = new ErrorMessage(
+                "An internal server error occurred. Please contact the administrator.",
+                Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()
+        );
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
 }
