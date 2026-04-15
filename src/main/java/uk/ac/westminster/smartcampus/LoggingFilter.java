0a2d4262a9f37a8eb5ab9cb9974533e234131040
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-15 20:31:59 +0530

    Create LoggingFilter.java

--- src/main/java/uk/ac/westminster/smartcampus/LoggingFilter.java
+++ src/main/java/uk/ac/westminster/smartcampus/LoggingFilter.java
@@ -1,4 +1,30 @@
 package uk.ac.westminster.smartcampus;
 
-public class LoggingFilter {
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter (ContainerRequestContext requestContext) throws IOException {
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+
+        LOGGER.info(">>> INCOMING REQUEST: " + method + " " + uri);
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        int status = responseContext.getStatus();
+
+        LOGGER.info("<<< OUTGOING RESPONSE: Status " + status);
+    }
 }
