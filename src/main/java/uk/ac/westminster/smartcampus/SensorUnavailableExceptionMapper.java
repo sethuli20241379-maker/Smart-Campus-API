0f8891c92d89c54a01f29c12c9e62252f1538de5
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-15 19:26:13 +0530

    Update SensorUnavailableExceptionMapper.java

--- src/main/java/uk/ac/westminster/smartcampus/SensorUnavailableExceptionMapper.java
+++ src/main/java/uk/ac/westminster/smartcampus/SensorUnavailableExceptionMapper.java
@@ -1,4 +1,9 @@
 package uk.ac.westminster.smartcampus;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
 public class SensorUnavailableExceptionMapper {
 }
