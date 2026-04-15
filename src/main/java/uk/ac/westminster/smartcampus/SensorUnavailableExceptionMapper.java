5c8ef43d1e98c13fc27a696362ec6cf13d540217
sethuli20241379-maker <sethuli.20241379@iit.ac.lk>
2026-04-15 19:29:38 +0530

    Update SensorUnavailableExceptionMapper.java

--- src/main/java/uk/ac/westminster/smartcampus/SensorUnavailableExceptionMapper.java
+++ src/main/java/uk/ac/westminster/smartcampus/SensorUnavailableExceptionMapper.java
@@ -5,5 +5,19 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-public class SensorUnavailableExceptionMapper {
+@Provider
+public class SensorUnavailableExceptionMapper implements ExceptionMapper<SensorUnavailableException> {
+
+    @Override
+    public Response toResponse(SensorUnavailableException exception) {
+        ErrorMessage error = new ErrorMessage(
+                exception.getMessage(),
+                Response.Status.FORBIDDEN.getStatusCode()
+        );
+
+        return Response.status(Response.Status.FORBIDDEN)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
 }
